72006529b8b7d91ae5db5ec9fe64c2209efaf751
edogawadoras1234 <edogawadoras1234@gmail.com>
2020-11-02 16:59:18 +0700

    add string resource

--- app/src/main/java/com/example/smartbandsdkdemo/ui/devicesetting/DeviceSettingActivity.java
+++ app/src/main/java/com/example/smartbandsdkdemo/ui/devicesetting/DeviceSettingActivity.java
@@ -12,11 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartbandsdkdemo.R;
 
+import java.util.Calendar;
+
 import cn.appscomm.bluetoothsdk.app.BluetoothSDK;
 import cn.appscomm.bluetoothsdk.interfaces.ResultCallBack;
 
 public class DeviceSettingActivity extends AppCompatActivity {
-    Button btn_pin, btn_version, btn_get_time, btn_set_time;
+    Button btn_pin, btn_version, btn_get_time, btn_set_time, btn_set_device_time;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,11 +32,15 @@ public class DeviceSettingActivity extends AppCompatActivity {
         btn_version = findViewById(R.id.button_get_device_version);
         btn_get_time = findViewById(R.id.button_get_device_time);
         btn_set_time = findViewById(R.id.button_set_time_format);
+        btn_set_device_time = findViewById(R.id.button_set_device_time);
 
         btn_pin.setOnClickListener(v -> BluetoothSDK.getBatteryPower(resultCallBack));
         btn_version.setOnClickListener(v -> BluetoothSDK.getDeviceVersion(resultCallBack));
         btn_get_time.setOnClickListener(v -> BluetoothSDK.getDeviceTime(resultCallBack));
         btn_set_time.setOnClickListener(v -> setting_time_format());
+        btn_set_device_time.setOnClickListener( v->{
+            BluetoothSDK.setDeviceTime(resultCallBack, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH) + 1, Calendar.getInstance().get(Calendar.DAY_OF_MONTH), Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE), Calendar.getInstance().get(Calendar.SECOND));
+        });
     }
 
     ResultCallBack resultCallBack = new ResultCallBack() {
@@ -61,6 +67,11 @@ public class DeviceSettingActivity extends AppCompatActivity {
                     Toast.makeText(DeviceSettingActivity.this, "Đã đặt lại định dạng thời gian!!!", Toast.LENGTH_SHORT).show();
                     Log.i(TAG, "Set time success!!!");
                     break;
+                case ResultCallBack.TYPE_SET_DEVICE_TIME:
+                    Toast.makeText(DeviceSettingActivity.this, "Đồng bộ thời gian với điện thoại!!!", Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "set time success!!!");
+                    break;
+
             }
         }
 
